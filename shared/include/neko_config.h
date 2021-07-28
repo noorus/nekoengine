8efd76848ac57eef73feca7c9299fb8626865939
noorus <noorus@users.noreply.github.com>
2021-07-28 15:33:44 +0300

    Add comments for new define toggles

--- shared/include/neko_config.h
+++ shared/include/neko_config.h
@@ -13,11 +13,18 @@
 #undef NEKO_VERBOSE_COMPILE
 
 // Set this to use doubles for all math primitives. Otherwise using floats.
+// Will most likely break things.
 #undef NEKO_MATH_DOUBLE
 
-// Set this to disable V8 entirely.
+// -- Feature switches --
+// The game/engine isn't meant to work properly without these, but it is
+// convenient to drop some dependencies when developing stuff on the road.
+
+// Define to disable scripting & drop the V8 libraries requirement.
 #define NEKO_NO_SCRIPTING
 
+// Define to disable audio & drop FMOD SDK requirement.
 #define NEKO_NO_AUDIO
 
+// Define to disable the GUI & drop MyGUI SDK requirement.
 #define NEKO_NO_GUI
